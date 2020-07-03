e9506c1c4edc31d84667340b15dbc4ad23aab2ca
dhmhtrhstasiopoulos <65129439+dhmhtrhstasiopoulos@users.noreply.github.com>
2020-07-03 04:14:27 +0300

    Add files via upload

---
+++ CalculateAges.java
@@ -0,0 +1,13 @@
+public class CalculateAges {
+
+  
+    public static void main(String[] args) {
+      System.out.println("My name is dimitris and i am : " + (2020-1960)+" years old and i will be " + (2040-1960) + " years old " );
+      System.out.println("My name is Giannhs and i am : " + (2020-1960)+" years old and i will be " + (2040-1960) + " years old " );
+      System.out.println("My name is Petros and i am : " + (2020-1960)+" years old and i will be " + (2040-1960) + " years old " );
+      System.out.println("My name is Nikos and i am : " + (2020-1960)+" years old and i will be " + (2040-1960) + " years old " );
+      
+      
+    }
+    
+}
